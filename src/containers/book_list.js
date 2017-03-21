7f8b1c909df331277ea5b677e719a45c1562511e
jamesv28 <james.volmert@davita.com>
2017-03-21 15:52:53 -0600

    added book list

---
+++ src/containers/book_list.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li className="list-group-item" key={book.title} > {book.title} </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}; // end of class
+
+function mapStateToProps(state) {
+  //  Whatever is returned here will go to this.props
+
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+//  making reducers so we can
+export default function () {
+  return [
+    {title: 'American Gods'},
+    {title: 'The Dark Tower'},
+    {title: 'Of Mice and Men'},
+    {title: 'Fantastic Beasts'},
+    {title: 'A Song of Ice and Fire'}
+
+  ]
+}
